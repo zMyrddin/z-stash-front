64635733397a7d00d4e868aa8c830b839441634e
zMyrddin <janmarvin.consul@gmail.com>
2023-12-27 22:13:37 +1100

    fix: reverted to working version for JWT

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,7 @@ function App() {
 return (
   <div>  
     <LoginForm />
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <Routes>
-      <Route path="/stashlist" element={<StashList />} />    
-    </Routes>
-=======
->>>>>>> parent of acfb0d1 (feat: playing around with AuthContext and UserContext to handle login context)
     <Navbar />
-=======
->>>>>>> parent of cabce16 (fix: Navbar unused bar added)
   </div>
 
 
--- src/functions/login.js
+++ src/functions/login.js
@@ -1,45 +1,23 @@
-<<<<<<< HEAD
-// login.js
-import { useAuth } from "../context/AuthContext";
-
-async function login(username, password) {
-  const auth = useAuth();
-
-  console.log(username, password);
-=======
 async function login(username, password){ 
-    console.log(username, password);
+  console.log(username, password);
 
-    let result = await fetch(
-      process.env.REACT_APP_BACKEND_URL + "/users/login",
-      {
-        method: "POST", // or 'PUT'
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({username: username, password: password}),
-      }
-    );
+  let result = await fetch(
+    process.env.REACT_APP_BACKEND_URL + "/users/login",
+    {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({username: username, password: password}),
+    }
+  );
 
-	let data = await result.json();
->>>>>>> parent of acfb0d1 (feat: playing around with AuthContext and UserContext to handle login context)
+let data = await result.json();
 
-    console.log(data);
-
-	return data;
-
-<<<<<<< HEAD
   console.log(data);
 
-  if (data.jwt) {
-    // Assuming the server sends a JWT and user data upon successful login
-    const { jwt, user } = data;
-    auth.login(jwt, user);
-  }
+return data;
 
-  return data;
-=======
->>>>>>> parent of acfb0d1 (feat: playing around with AuthContext and UserContext to handle login context)
 }
 
 module.exports = {login}
